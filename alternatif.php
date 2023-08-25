aa4bd1d78d14217979fdc4f3230f746bea2707d6
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-08-25 22:09:43 +0700

    added connection to database anf added alternatif.php view and query data from table alternatif

--- alternatif.php
+++ alternatif.php
@@ -13,7 +13,68 @@ createHeader("Alternatif");
     $body = function () { ?>
         <!-- Page Content  -->
         <div id="content" class="p-4 p-md-5 pt-5">
-            <h2 class="mb-4">Empty</h2>
+            <div class="row">
+                <h2 class="mb-4 text-capitalize">lahan alternatif</h2>
+                <div class=" ml-auto d-flex mb-3 pt-1 pb-1" style="margin-right: 10%;">
+                    <button type="button" class="btn btn-warning">Tambah</button>
+                </div>
+            </div>
+            <!-- start table -->
+            <table class="table table-hover">
+                <thead class="text-capitalize">
+                    <th>no</th>
+                    <th>nama pemilik</th>
+                    <th>alamat</th>
+                    <th>jenis tanah</th>
+                    <th>suhu</th>
+                    <th>ketersediaan air</th>
+                    <th>PH tanah</th>
+                    <th>lapisan olahan</th>
+                    <th>aksi</th>
+                </thead>
+                <tbody>
+                    <?php
+                    include("db.php");
+                    $get_list_kriteria = "SELECT a.id_alternatif, a.nama_pemilik, alamat, a.c1, a.c2, a.c3, a.c4, a.c5 FROM alternatif a";
+                    $list_kriteria = $conn->query($get_list_kriteria);
+
+                    $get_crips = "SELECT nama FROM crips WHERE id_crips = ";
+
+                    foreach ($list_kriteria as $i => $value) {
+                        $c1 = $conn->query($get_crips . $value["c1"]);
+                        $c1 = $c1->fetch_array();
+
+                        $c2 = $conn->query($get_crips . $value["c2"]);
+                        $c2 = $c2->fetch_array();
+
+                        $c3 = $conn->query($get_crips . $value["c3"]);
+                        $c3 = $c3->fetch_array();
+
+                        $c4 = $conn->query($get_crips . $value["c4"]);
+                        $c4 = $c4->fetch_array();
+
+                        $c5 = $conn->query($get_crips . $value["c5"]);
+                        $c5 = $c5->fetch_array();
+                    ?>
+                        <tr>
+                            <td><?= ($i + 1) ?></td>
+                            <td><?= $value["nama_pemilik"] ?></td>
+                            <td><?= $value["alamat"] ?></td>
+                            <td><?= $c1["nama"] ?></td>
+                            <td><?= $c2["nama"] ?></td>
+                            <td><?= $c3["nama"] ?></td>
+                            <td><?= $c4["nama"] ?></td>
+                            <td><?= $c5["nama"] ?></td>
+                            <td>
+                                <button type="button" class="btn btn-outline-success">Edit</button>
+                                <button type="button" class="btn btn-danger">Hapus</button>
+                            </td>
+                        </tr>
+                    <?php }
+                    ?>
+                </tbody>
+            </table>
+            <!-- end table -->
         <?php };
 
     createSidebar($body); ?>
