d086bc7515f0437336de04ca918f196454d3facc
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-08-26 02:29:41 +0700

    added table matriks ternormalisasi

--- topsis.php
+++ topsis.php
@@ -11,7 +11,9 @@ createHeader("Topsis");
     createNavbar();
 
     $body = function () {
-        include("db.php"); ?>
+        include("db.php");
+        $crips_global = [];
+    ?>
         <!-- Page Content  -->
         <div id="content" class="p-4 p-md-5 pt-5">
             <h2 class="mb-4">Topsis</h2>
@@ -119,7 +121,17 @@ createHeader("Topsis");
                     foreach ($list_alternatif as $i => $v) {
                         $c5_temp = $conn->query($get_crips . $v["c5"])->fetch_assoc();
                         $c5 += pow(floatval($c5_temp["bobot"]), 2);
-                    } ?>
+                    }
+
+                    // set all crips to global variable
+                    $crips_global = [
+                        "c1" => $c1,
+                        "c2" => $c2,
+                        "c3" => $c3,
+                        "c4" => $c4,
+                        "c5" => $c5,
+                    ];
+                    ?>
                     <tr>
                         <td><?= $c1 ?></td>
                         <td><?= $c2 ?></td>
@@ -130,6 +142,55 @@ createHeader("Topsis");
                 </tbody>
             </table>
             <!-- start pembagian -->
+
+            <!-- start matriks ternormalisasi -->
+            <h3 class="mb-4 text-center text-capitalize">matriks ternormalisasi</h3>
+            <table class="table table-hover shadow">
+                <thead class="text-capitalize">
+                    <th>no</th>
+                    <th>nama pemilik</th>
+                    <th>alamat</th>
+                    <th>jenis tanah</th>
+                    <th>suhu</th>
+                    <th>ketersediaan air</th>
+                    <th>PH tanah</th>
+                    <th>lapisan olahan</th>
+                </thead>
+                <tbody>
+                    <?php
+                    $get_list_alternatif = "SELECT a.id_alternatif, a.nama_pemilik, alamat, a.c1, a.c2, a.c2_real, a.c3, a.c4, a.c4_real, a.c5 FROM alternatif a";
+                    $list_alternatif = $conn->query($get_list_alternatif);
+                    $get_crips = "SELECT bobot FROM crips WHERE id_crips = ";
+
+                    foreach ($list_alternatif as $i => $value) {
+                        $c1 = $conn->query($get_crips . $value["c1"]);
+                        $c1 = $c1->fetch_array();
+
+                        // $c2 = $conn->query($get_crips . $value["c2"]);
+                        // $c2 = $c2->fetch_array();
+
+                        $c3 = $conn->query($get_crips . $value["c3"]);
+                        $c3 = $c3->fetch_array();
+
+                        // $c4 = $conn->query($get_crips . $value["c4"]);
+                        // $c4 = $c4->fetch_array();
+
+                        $c5 = $conn->query($get_crips . $value["c5"]);
+                        $c5 = $c5->fetch_array(); ?>
+                        <tr>
+                            <td><?= ($i + 1) ?></td>
+                            <td><?= $value["nama_pemilik"] ?></td>
+                            <td><?= $value["alamat"] ?></td>
+                            <td><?= ($crips_global["c1"] == 0 ? 0 : $c1["bobot"] / $crips_global["c1"]) ?></td>
+                            <td><?= ($crips_global["c2"] == 0 ? 0 : $value["c2"] / $crips_global["c2"]) ?></td>
+                            <td><?= ($crips_global["c3"] == 0 ? 0 : $c3["bobot"] / $crips_global["c3"]) ?></td>
+                            <td><?= ($crips_global["c4"] == 0 ? 0 : $value["c4"] / $crips_global["c4"]) ?></td>
+                            <td><?= ($crips_global["c5"] == 0 ? 0 : $c5["bobot"] / $crips_global["c5"]) ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+            <!-- end matriks ternormalisasi -->
         <?php };
 
     createSidebar($body); ?>
