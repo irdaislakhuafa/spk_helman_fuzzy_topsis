e0003b07dcddfe6065156f6619a8208db616042b
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-08-25 21:20:10 +0700

    added template files

---
+++ fuzzy.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("basic.php");
+createHeader("Fuzzy");
+?>
+
+
+<body>
+    <?php
+    createNavbar();
+
+    $body = function () { ?>
+        <!-- Page Content  -->
+        <div id="content" class="p-4 p-md-5 pt-5">
+            <h2 class="mb-4">Empty</h2>
+        <?php };
+
+    createSidebar($body); ?>
+</body>
+
+<?php
+createFooter();
+?>
+
+</html>
