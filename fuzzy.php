e39378970c6af8d510ffc3b87a2c0ce9eec33f1e
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-08-26 22:23:22 +0700

    fix revision

--- fuzzy.php
+++ fuzzy.php
@@ -29,7 +29,7 @@ createHeader("Fuzzy");
                 <tbody>
                     <?php
                     include("db.php");
-                    $get_list_alternatif = "SELECT a.id_alternatif, a.nama_pemilik, alamat, a.c1, a.c2, a.c2_real, a.c3, a.c4, a.c4_real, a.c5 FROM alternatif a";
+                    $get_list_alternatif = "SELECT a.id_alternatif, a.nama_pemilik, alamat, a.c1, a.c2, a.c3, a.c4, a.c5 FROM alternatif a";
                     $list_alternatif = $conn->query($get_list_alternatif);
 
                     $get_crips = "SELECT bobot FROM crips WHERE id_crips = ";
@@ -38,14 +38,14 @@ createHeader("Fuzzy");
                         $c1 = $conn->query($get_crips . $value["c1"]);
                         $c1 = $c1->fetch_array();
 
-                        // $c2 = $conn->query($get_crips . $value["c2"]);
-                        // $c2 = $c2->fetch_array();
+                        $c2 = $conn->query($get_crips . $value["c2"]);
+                        $c2 = $c2->fetch_array();
 
                         $c3 = $conn->query($get_crips . $value["c3"]);
                         $c3 = $c3->fetch_array();
 
-                        // $c4 = $conn->query($get_crips . $value["c4"]);
-                        // $c4 = $c4->fetch_array();
+                        $c4 = $conn->query($get_crips . $value["c4"]);
+                        $c4 = $c4->fetch_array();
 
                         $c5 = $conn->query($get_crips . $value["c5"]);
                         $c5 = $c5->fetch_array();
@@ -55,9 +55,9 @@ createHeader("Fuzzy");
                             <td><?= $value["nama_pemilik"] ?></td>
                             <td><?= $value["alamat"] ?></td>
                             <td><?= $c1["bobot"] ?></td>
-                            <td><?= $value["c2"] ?></td>
+                            <td><?= $c2["bobot"] ?></td>
                             <td><?= $c3["bobot"] ?></td>
-                            <td><?= $value["c4"] ?></td>
+                            <td><?= $c4["bobot"] ?></td>
                             <td><?= $c5["bobot"] ?></td>
                         </tr>
                     <?php }
